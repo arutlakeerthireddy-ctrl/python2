960b413479402efae90a5bee7df0f1688fca8832
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-05-26 22:21:26 +0530

    added

---
+++ python dates/dates.py
@@ -0,0 +1,7 @@
+#python Dates:A date in python is not a datatype of its own,but we can import a module named datetime to work with dates as dates objects
+#example:
+import datetime
+x=datetime.datetime.now()
+print(x)
+
+#2026-05-26 22:18:10.365106
