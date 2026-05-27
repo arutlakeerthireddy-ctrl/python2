25baa5f962cf5be5485027271f42a2d468794436
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-05-27 19:09:01 +0530

    added math module

---
+++ Math/math.py
@@ -0,0 +1,39 @@
+'''python has a set of buitin math functions,including an extensive math module,that allows you to perform mathematical tasks on numbers'''
+#Built-in math functions
+#min() and max() functions used to find lowest and highest value in an iterable
+x=min(3,9,1)
+y=max(7,9,2)
+print(x)#1
+print(y)#9
+
+#abs():returns absolute (positive) value of the specified number
+x=abs(-89.7)
+print(x)#89.7
+
+#pow(x,y):function returns value of x to the power of y(x**y)
+x=pow(5,2)
+print(x)#25
+
+#math module:it is a python buitin module which extends list of mathematical functions
+#to use it ,we must import math module
+import math
+#methods and constants of module
+#math.sqrt():returns square root of a number
+import math
+x=math.sqrt(81)
+print(x)#9.0
+
+#math.ceil():method rounds a number upwards to its nearest integer
+import math
+x=math.ceil(1.3)
+print(x)#2
+
+#math.floor():method rounds a number downwards to its nearest number and returns result
+y=math.floor(1.3)
+print(y)#1
+
+#math.pi:returns the constant value of PI(3.14)
+import math
+x=math.pi
+print(x)#3.141592653589793
+
