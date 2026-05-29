7392a2ef69a1a6fb11bcb3150dda1e3d805557c7
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-05-29 21:32:50 +0530

    added

--- python JSON/program.py
+++ python JSON/program.py
@@ -1,6 +1,7 @@
 #Add New Data to JSON
 import json
 student={"Name":"keerthi","roll":14}
-student["course"]="CSM"
+student["course"]="Btech"
+student["Branch"]="csm"
 x=json.dumps(student)
 print(x)
