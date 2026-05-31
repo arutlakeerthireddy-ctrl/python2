6bc85ab98c6975b938ec8f5502139fd4348511c5
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-05-31 23:52:13 +0530

    added

--- python JSON/programs.py
+++ python JSON/programs.py
@@ -2,3 +2,8 @@ import json
 json_string ='{"name":"Keerthi","age":20,"city":"Hyderabad"}'
 data=json.loads(json_string)
 print(data["city"])
+
+import json
+a='"hloo"'
+data=json.loads(a)
+print(data)
