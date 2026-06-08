0ddb63914bf5986f284bae058682b934f2994187
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-06-08 23:24:44 +0530

    added

--- python JSON/progs.py
+++ python JSON/progs.py
@@ -9,3 +9,13 @@ data={"fruit":"Apple","price":50}
 x=json.dumps(data)
 print(x)
 
+import json
+data = {
+    "name": "Keerthi",
+    "age": 21,
+    "city": "Suryapet"
+}
+
+json_data = json.dumps(data, indent=4)
+print(json_data)
+
